9fa09fb28816df0011ba81874e001adbf9f730ba
hkohko <hekodev.py@gmail.com>
2024-02-05 12:25:50 +0700

    minor refactor and created test dir

--- src/bfs.rs
+++ src/bfs.rs
@@ -3,7 +3,7 @@ use std::collections::{VecDeque, HashMap};
 use std::fs::{self, ReadDir};
 use std::path::{Path, PathBuf};
 pub fn bfs_main() {
-    let start = PathBuf::from("/home/user/Documents/resume/programmer");
+    let start = PathBuf::from("/home/user/Documents/rust_proj/dsa_made_easy/test_dir/");
     let _ = bfs(start);
 }
 fn bfs(start: PathBuf) {
@@ -14,20 +14,17 @@ fn bfs(start: PathBuf) {
 
     while deque.len() > 0 {
         let current_node = deque.pop_front();
-        match current_node {
-            None => (),
-            Some(path) => {
-                println!("{:?}", &path);
-                visited_vertices.insert(path.clone(), true);
-                if path.is_dir() {
-                    let nodes = std::fs::read_dir(path).unwrap();
-                    for node in nodes {
-                        let node_pathbuf = node.unwrap().path();
-                        if let Some(true) = visited_vertices.get(&node_pathbuf) {
-                            continue
-                        }
-                        deque.push_back(node_pathbuf);
+        if let Some(path) = current_node {
+            println!("{:?}", &path);
+            visited_vertices.insert(path.clone(), true);
+            if path.is_dir() {
+                let nodes = std::fs::read_dir(path).unwrap();
+                for node in nodes {
+                    let node_pathbuf = node.unwrap().path();
+                    if let Some(true) = visited_vertices.get(&node_pathbuf) {
+                        continue
                     }
+                    deque.push_back(node_pathbuf);
                 }
             }
         }
